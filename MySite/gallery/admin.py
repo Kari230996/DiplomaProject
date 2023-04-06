5d3849c18ec2ef3a7ae4a17425f7c8252f14e7da
Kari230996 <claro.claire1996@gmail.com>
2023-04-06 18:04:37 +0400

    castomized admin

--- MySite/gallery/admin.py
+++ MySite/gallery/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
 from .models import Gallery
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
 class GalleryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published',)
+  
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return "-"
+        
 
+    get_photo.short_description = 'Photo'
 
 admin.site.register(Gallery, GalleryAdmin)
+
+admin.site.site_title = 'Gallery settings'
+admin.site.site_header = 'Gallery settings'
 
