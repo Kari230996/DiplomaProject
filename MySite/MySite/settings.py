0cb44d843d95c298daa4e422fb58b7efa5a2df79
Karina <karina.apaeva96@gmail.com>
2026-05-06 19:49:04 +0300

    fix: fixed settings

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-glczcb1h--9p=v#-3v00e4ds^2cn!&py&0^9_f!x-f^18$ev08'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -255,10 +259,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'karina.apaeva96@gmail.com'
-EMAIL_HOST_PASSWORD = 'fisxyozkerzzjwnb'
-DEFAULT_FROM_EMAIL = 'karina.apaeva96@gmail.com'
-MY_EMAIL_ADDRESS = 'karina.apaeva96@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+MY_EMAIL_ADDRESS = os.getenv('MY_EMAIL_ADDRESS')
 
 
 
